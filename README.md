346c7d23dcd3471b22299f28c7af4eedd4493dc5
Devante <devante.jones229@gmail.com>
2019-11-13 21:28:42 -0500

    Added to README file, gathered screenshots of the app, set default cases for movie-this and spotify-this-song

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # liri-node-app
+
+LIRI is like iPhone's SIRI. However, instead of using voice recognition, like SIRI, Liri
+interprets language and returns data. 
+
+LIRI takes in 1 of 4 commands for its first argument:
+
+1. `concert-this``<artist/band name>`
+
+    - Searches the SeatGeek Artist Events API for an artist and renders relevant information: Venue name & location and date of the event
+
+2. `movie-this``<movie name>`
+
+    - Retrieves data from the OMDB API for that displays the following for the desired movie: Title, release year, ratings, actors, plot, etc.
+
+3. `spotify-this-song` `<song name>`
+
+    - Uses the Spotify API to retrieve the information associated with the song name: artist name, album for the song, preview link, and name of song
+
+4. `do-what-it-says` 
+
+    - Calls on 1 of 3 functions listed above, depending on what command is in the random.txt file
+
+
--- liri.js
+++ liri.js
@@ -1,3 +1,5 @@
+// Importing needed packages
+
 var dotenv = require("dotenv").config();
 const Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
@@ -6,23 +8,29 @@ var moment = require("moment");
 var fs = require("fs");
 
 
-// var seatGeek = new seatGeek(keys.seatGeek);
 var spotify = new Spotify(keys.spotify);
 var omdb = keys.omdb.id;
 var seatGeek = keys.seatGeek.id;
 
 var action = process.argv[2];
-var value = process.argv.slice(3).join("+");
+var movie = process.argv.slice(3).join("+");
 var song = process.argv.slice(3).join(" ")
 var performer = process.argv.slice(3).join("-");
 
+// Setting default song to display if no song is entered
 if (!song) {
-    song = "The Sign"
+    song = "The Sign";
 }
 
+// Setting default movie to display if no title is entered
+if (!movie){
+    movie = "Mr. Nobody";
+}
+
+// Conditions for desired function to be ran
 switch (action) {
     case "concert-this":
-        concert();
+        getConcert();
         break;
 
     case "spotify-this-song":
@@ -30,7 +38,7 @@ switch (action) {
         break;
 
     case "movie-this":
-        movie();
+        getMovie();
         break;
 
     case "do-what-it-says":
@@ -38,9 +46,9 @@ switch (action) {
         break;
 }
 
-
-function movie() {
-    axios.get("http://www.omdbapi.com/?t=" + value + "&apikey=" + omdb)
+// Function retrieves info from OMDB API based on the movie title
+function getMovie() {
+    axios.get("http://www.omdbapi.com/?t=" + movie + "&apikey=" + omdb)
         .then(function (response) {
             const movieData = response.data;
             console.log(`Title: ${movieData.Title}
@@ -58,8 +66,9 @@ Plot: ${movieData.Plot}`);
         })
 }
 
-function concert() {
-    axios.get(`https://api.seatgeek.com/2/events?performers.slug=${performer}&client_id=${seatGeek}`)
+// concert-this function to retrieve info from SeatGeek
+function getConcert() {
+    return axios.get(`https://api.seatgeek.com/2/events?performers.slug=${performer}&client_id=${seatGeek}`)
         .then(function (response) {
             const venueInfo = response.data.events[0].venue;
             const date = moment(response.data.events[0].datetime_local).format('L');
@@ -70,10 +79,10 @@ Date: ${date}`);
             if (err.response) {
                 console.log(err);
             }
-        })
-
+        });
 }
 
+// Function that retrieves data from Spotify API based on the song entered
 function getSong() {
     spotify.search({ type: 'track', query: song }, function (err, data) {
         if (err) {
@@ -91,20 +100,28 @@ function getSong() {
 
 }
 
-function doThis(){
+// Function reads the text in random.txt and parses the text into an array
+// Uses index 0 and 1 to run 1 of the 3 above functions
+function doThis() {
 
-    fs.readFile("random.txt", "utf8", function(err, data) {
+    fs.readFile("random.txt", "utf8", function (err, data) {
         if (err) {
-          return console.log(err);
+            return console.log(err);
         }
         data = data.split(", ");
 
-        if (data[0] === "spotify-this-song"){
+        if (data[0] === "spotify-this-song") {
             song = data[1];
             getSong();
+        } else if (data[0] === "concert-this") {
+            performer = data[1];
+            getConcert();
+
+        } else if (data[0] === "movie-this") {
+            movie = data[1];
+            getMovie();
         }
 
-        console.log(data);
-});
+    });
 
 }
